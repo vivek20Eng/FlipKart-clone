755b806d71add453467ecdab4aef0b9f0414730f
rishabharidas <rishabharidas010@gmail.com>
2024-06-19 11:52:34 +0530

    resolve conflicts

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,11 +3,14 @@ import Header from './Header'
 import CarouselHead from './CarouselHead'
 import SideNav from './SideNav'
 import HomeDeals from './HomeDeals'
+import Carsosel from './Carsosel'
 
 export default function ProductList() {
   return (
     <div className=''>
       <Header/>
+      <Carsosel/>
+      
       {/* <CarouselHead/>  */}
       <HomeDeals/>
       </div>
